557c22a369e4f8bd61963b5dddd333296b5b372f
mrideout1985 <mrideout1985@gmail.com>
2020-12-23 06:02:36 +0000

    added new main header :ok_hand:

--- components/mainheader/mainheader.js
+++ components/mainheader/mainheader.js
@@ -2,9 +2,18 @@ import React from "react";
 import ToggleButton from "../ToggleButton/toggleButton";
 import Link from "next/link";
 import styles from "./mainheader.module.scss";
+import { EndTag, Tag } from "../../components/icons/svg/index";
 
 const Mainheader = () => {
-	return <div className={styles.header}>`</div>;
+	return (
+		<div className={styles.header}>
+			<div class={styles["logo"]}>
+				<Tag height={5} stroke={35} />
+				<h1>Matthew Rideout</h1>
+				<EndTag height={5} stroke={35} />
+			</div>
+		</div>
+	);
 };
 
 export { Mainheader };
