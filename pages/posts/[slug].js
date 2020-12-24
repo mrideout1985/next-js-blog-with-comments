14b2c7b0c226bb1fad2e3d1965f6e430bd8ece07
mrideout1985 <mrideout1985@gmail.com>
2020-12-24 14:37:17 +0000

    new header :rainbow:

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -41,10 +41,8 @@ export default function Post({ post, morePosts, preview }) {
 							/>
 							<PostBody content={post.body} />
 						</article>
-
 						<Comments comments={post.comments} />
 						<Form _id={post._id} />
-
 						<SectionSeparator />
 						{morePosts.length > 0 && (
 							<MoreStories posts={morePosts} />
